2d43621758648798c2e92db6c205097e98c2b55a
2BuRy1 <kot514877@gmail.com>
2025-02-21 20:31:36 +0300

    some fixes, main algo and exceptions left

--- solution/calculations.py
+++ solution/calculations.py
@@ -7,8 +7,11 @@ def make_C_and_D_matrix(matrix):
         a_ii = matrix[i][i]
         print(f"Диагональный элемент a_{i}{i}: {a_ii}")
 
-        C_row = [(-matrix[i][j] / a_ii) for j in range(len(matrix)) if j != i]
+        C_row = [0 if i == j else (-matrix[i][j] / a_ii) for j in range(len(matrix) + 1)]
         C.append(C_row)
-        D_row = matrix[i][len(matrix)]
+        D_row = (matrix[i][len(matrix)])/a_ii
         D.append(D_row)
     return C, D
+
+
+def main_calculation(C, D, matrix):
